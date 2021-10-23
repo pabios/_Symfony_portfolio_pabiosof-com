fc8577ec762aa9b399a9ef67da53a28a07632f67
pabios <pabios.pabiosoft@gmail.com>
2021-10-24 01:22:10 +0200

    ajout de boostrap et resolution  du boeug page edit champ vide

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -53,7 +53,7 @@ class Pin
         return $this->title;
     }
 
-    public function setTitle(string $title): self
+    public function setTitle(?string $title): self
     {
         $this->title = $title;
 
@@ -65,7 +65,7 @@ class Pin
         return $this->description;
     }
 
-    public function setDescription(string $description): self
+    public function setDescription(?string $description): self
     {
         $this->description = $description;
 
