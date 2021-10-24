16c2e471cfd22e4e557f5c2e05574acf5182566f
pabios <pabios.pabiosoft@gmail.com>
2021-10-24 13:27:44 +0200

    ok pour l' ajout de l'image

--- src/Form/PinType.php
+++ src/Form/PinType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Pin;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PinType extends AbstractType
@@ -12,6 +13,16 @@ class PinType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('imageFile', VichImageType::class, [
+                'label' => 'Image JPG ou PNG',
+                'required' => false,
+                'allow_delete' => true,
+                'delete_label' => 'Supprimer l\'image',
+                'download_label' => 'Telecharger',
+                'download_uri' => true,
+                'image_uri' => true,
+                'asset_helper' => true,
+            ])
             ->add('title')
             ->add('description')
         ;
