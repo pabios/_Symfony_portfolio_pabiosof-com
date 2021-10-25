e70d74ea0bfd5032e113a2efd675d96c231a1d82
pabios <pabios.pabiosoft@gmail.com>
2021-10-25 14:12:03 +0200

    page affichage show

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -47,7 +47,7 @@ class Pin
      * NOTE: This is not a mapped field of entity metadata, just a simple property.
      * 
      * @Vich\UploadableField(mapping="pin_image", fileNameProperty="imageName")
-     * @Assert\NotNull(message="Ajouter une image svp")
+     *  ==>Assert\NotNull(message="Ajouter une image svp")
      * @Assert\Image(maxSize="10M",maxSizeMessage="le fichier est trop grand")
      * @var File|null
      */
